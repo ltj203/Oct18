cd1b8bea586b423f4e8cd367a6e01586602826c1
Lisa <ljenkin2@hotmail.com>
2012-10-18 09:10:37 -0400

    first project

---
+++ Project/ProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProjectAppDelegate.h
+//  Project
+//
+//  Created by Lisa Jenkins on 10/18/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
